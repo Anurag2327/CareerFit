386ef1c9b023b6ab0ae16a35fe1d2f82fd8756e7
Anurag2327 <anuragpandey2265@gmail.com>
2026-09-21 05:29:21 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+require("dotenv").config();
+
+const app = require("./src/app");
+const connectToDB = require("./src/config/db");
+
+connectToDB();
+
+
+app.listen(3000, ()=>{
+    console.log("server is running on port 3000")
+});
